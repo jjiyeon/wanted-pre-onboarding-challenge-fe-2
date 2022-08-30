df5fe31c95b7ac5d5e49e0d871b094c1673ee6b2
Journey <ejy1105@gmail.com>
2022-08-30 22:27:54 +0900

    feat: todoList 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * Todo type
+ * Todo 할일 type
  * @typedef {Object} Todo
  * @property {number} id - id
  * @property {string} text - 할일
@@ -31,7 +31,7 @@ TodoApp.prototype.setState = function (state) {};
  */
 /**
  * 할일 삭제 콜백함수
- * @callback removeAll
+ * @callback deleteAll
  * @returns {boolean}
  */
 
@@ -40,10 +40,10 @@ TodoApp.prototype.setState = function (state) {};
  * @constructor
  * @param {string} element
  * @param {onCreate} callback
- * @param {removeAll} removeAll
+ * @param {deleteAll} removeAll
  */
 
-function TodoItem(element, onCreate, removeAll) {}
+function TodoItem(element, onCreate, deleteAll) {}
 /**
  * 할일 컴포넌트를 render 함수
  */
@@ -54,14 +54,48 @@ TodoItem.prototype.render = function () {};
  * @param {string} category
  * @param {string[]} tags
  */
-TodoItem.prototype.onCreate = function (text, category, tags) {};
+TodoItem.prototype.todoCreate = function (text, category, tags) {};
 /**
  * 할일 전체 삭제
  */
-TodoItem.prototype.removeAll = function () {};
+TodoItem.prototype.deleteAll = function () {};
+
+/**
+ * 전체 할일 리스트 가져오기
+ * @callback getList
+ * @returns {Todo[]}
+ */
+/**
+ * 특정 id 할일 삭제
+ * @callback onDelete
+ * @returns {boolean}
+ */
+/**
+ * 특정 id 할일/태그 수정(삭제)
+ * @callback onUpdate
+ * @returns {Todo}
+ */
 
 /**
  * 할일 리스트
  * @constructor
+ * @param {Todo[]} todoList
+ * @param {onDelete} onDelete
+ * @param {onUpdate} onUpdate
  */
-function TodoList() {}
+function TodoList(todoList, onDelete, onUpdate) {}
+/**
+ * 할일 리스트 그리기
+ * @param {Todo[]} todoList
+ */
+TodoList.prototype.render = function (todoList) {};
+/**
+ * 특정 id 할일/태그 수정
+ * @param {number} todoId
+ */
+TodoList.prototype.todoUpdate = function (todoId) {};
+/**
+ * 특정 id 할일 삭제
+ * @param {number} todoId
+ */
+TodoList.prototype.todoDelete = function (todoId) {};
